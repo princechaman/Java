0fca2ed0288c0556565576ec4535c009f7753fd6
princechaman <2021pappu2003@gmail.com>
2023-05-06 11:59:58 +0530

    simple interst

--- Demo.java
+++ Demo.java
@@ -1,7 +1,25 @@
-class Demo 
+import java.util.Scanner;
+class  Swapp
 {
     public static void main (String arr[])
 	{
-	    System.out.print("Hello World"); 	
+		int a,b;
+        Scanner in = new Scanner(System.in);	
+	    System.out.print("Enter first number  :");
+        a = in.nextInt();
+		
+		System.out.print("Enter second number  :");
+        b = in.nextInt();
+
+        System.out.print("Before Swapping ");
+        System.out.print("a ="+a+" b ="+b);
+        
+		a= a+b;
+		b=a-b;
+		a=a-b;
+        System.out.print("After Swapping ");
+        System.out.print("a ="+a+" b ="+b);
 	}
-}
+} 
+    
+
